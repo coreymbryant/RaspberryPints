c63c308bedbc842a23c02f69e3c9a3aa0674692f
Corey M Bryant <corey.m.bryant@gmail.com>
2021-04-13 21:33:49 -0500

    Fixes update script to use mysqli

--- admin/update_column.php
+++ admin/update_column.php
@@ -15,10 +15,14 @@ $config_Value=$_POST['configValue'];
 
 foreach($_POST as $k => $v){
 	// update data in mysql database
-	$stmt = $conn->prepare("UPDATE config SET configValue=:configValue WHERE id=:id");
-	$stmt->bindParam(':configValue', $v, PDO::PARAM_STR);
-	$stmt->bindParam(':id', $k, PDO::PARAM_STR);
-	$result = $stmt->execute();
+	$stmt = mysqli_prepare($conn,"UPDATE config SET configValue=? WHERE id=?");
+    mysqli_stmt_bind_param($stmt,"si",$v,$k);
+    mysqli_stmt_execute($stmt);
+    // $result = mysqli_stmt_get_result($stmt);
+	// $stmt = $conn->prepare("UPDATE config SET configValue=:configValue WHERE id=:id");
+	// $stmt->bindParam(':configValue', $v, PDO::PARAM_STR);
+	// $stmt->bindParam(':id', $k, PDO::PARAM_STR);
+	// $result = $stmt->execute();
 }
 
 
